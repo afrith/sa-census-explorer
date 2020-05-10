62ca22f1c923bec3cf3b444ff1c233ab96507048
Adrian Frith <adrian@adrianfrith.com>
2020-05-10 17:01:27 +0200

    Add line layers

--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,77 @@
 import React, { useState, useEffect } from 'react'
-import ReactMapGL, { WebMercatorViewport } from 'react-map-gl'
+import ReactMapGL, { Source, Layer, WebMercatorViewport } from 'react-map-gl'
 import queryString from 'query-string'
 import 'mapbox-gl/dist/mapbox-gl.css'
+
+const layout = {
+  'line-cap': 'round',
+  'line-join': 'round'
+}
+
+const layers = [
+  {
+    id: 'subplace-line',
+    'source-layer': 'subplace',
+    minzoom: 9,
+    type: 'line',
+    layout,
+    paint: {
+      'line-color': '#4daf4a',
+      'line-width': 0.2
+    }
+  },
+  {
+    id: 'mainplace-line',
+    'source-layer': 'mainplace',
+    minzoom: 7,
+    type: 'line',
+    layout,
+    paint: {
+      'line-color': '#4daf4a',
+      'line-width': 0.5
+    }
+  },
+  {
+    id: 'local-line',
+    'source-layer': 'local',
+    type: 'line',
+    layout,
+    paint: {
+      'line-color': '#377eb8',
+      'line-width': 1
+    }
+  },
+  {
+    id: 'district-line',
+    'source-layer': 'district',
+    type: 'line',
+    layout,
+    paint: {
+      'line-color': '#1f78b4',
+      'line-width': 2
+    }
+  },
+  {
+    id: 'metro-line',
+    'source-layer': 'metro',
+    type: 'line',
+    layout,
+    paint: {
+      'line-color': '#377eb8',
+      'line-width': 2
+    }
+  },
+  {
+    id: 'province-line',
+    'source-layer': 'province',
+    type: 'line',
+    layout,
+    paint: {
+      'line-color': '#000000',
+      'line-width': 3
+    }
+  }
+]
 
 const calculateInitialViewport = ({ width, height }) => {
   const hash = queryString.parse(window.location.hash)
@@ -52,6 +122,10 @@ export default function Map () {
       width='100%'
       height='100%'
       onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+    >
+      <Source id='census' type='vector' tiles={[`${process.env.REACT_APP_TILE_ROOT}/{z}/{x}/{y}.mvt`]} maxzoom={20}>
+        {layers.map(l => <Layer key={l.id} {...l} />)}
+      </Source>
+    </ReactMapGL>
   )
 }
