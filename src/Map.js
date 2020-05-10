97d524f3e4b0b0867b2ea99c1a8f89d813ce30d7
Adrian Frith <adrian@adrianfrith.com>
2020-05-10 16:59:51 +0200

    Initialise map position from hash if present, and update hash when viewport changes

--- src/Map.js
+++ src/Map.js
@@ -1,28 +1,49 @@
 import React, { useState, useEffect } from 'react'
 import ReactMapGL, { WebMercatorViewport } from 'react-map-gl'
+import queryString from 'query-string'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
-export default function Map () {
-  const [viewport, setViewport] = useState({
-    latitude: -30,
-    longitude: 25,
-    zoom: 6
+const calculateInitialViewport = ({ width, height }) => {
+  const hash = queryString.parse(window.location.hash)
+  let latitude = parseFloat(hash.lat)
+  let longitude = parseFloat(hash.lon)
+  let zoom = parseFloat(hash.zoom)
+
+  if (isNaN(latitude) || isNaN(longitude) || isNaN(zoom)) {
+    ({ longitude, latitude, zoom } = new WebMercatorViewport({ width, height })
+      .fitBounds([[16.45, -34.833333], [32.891667, -22.127778]], { padding: 20 }))
+  }
+
+  return { longitude, latitude, zoom }
+}
+
+const updateHash = ({ longitude, latitude, zoom }) => {
+  const newHash = '#' + queryString.stringify({
+    lat: latitude.toFixed(3),
+    lon: longitude.toFixed(3),
+    zoom: zoom.toFixed(1)
   })
+  window.history.replaceState(null, null, newHash)
+}
 
-  const { width, height } = viewport
+export default function Map () {
+  const [viewport, setViewport] = useState({})
+  const [locationInitialised, setLocationInitialised] = useState(false)
 
-  // Once the initial size of the map is known, position it to show the whole of SA
+  // Once the initial size of the map is known, position it to hash if it exists, or to fit whole SA
   useEffect(() => {
-    if (!height) return
-    const { longitude, latitude, zoom } = new WebMercatorViewport({ width, height })
-      .fitBounds([[16.45, -34.833333], [32.891667, -22.127778]], { padding: 20 })
-    setViewport(viewport => ({
-      ...viewport,
-      longitude,
-      latitude,
-      zoom
-    }))
-  }, [!height]) // eslint-disable-line
+    if (locationInitialised || !viewport.height || !viewport.width) return
+
+    const newViewport = calculateInitialViewport(viewport)
+    setViewport(viewport => ({ ...viewport, ...newViewport }))
+    setLocationInitialised(true)
+  }, [viewport, locationInitialised])
+
+  // Debounced hash updates
+  useEffect(() => {
+    const timeout = setTimeout(() => { updateHash(viewport) }, 250)
+    return () => { clearTimeout(timeout) }
+  }, [viewport])
 
   return (
     <ReactMapGL
